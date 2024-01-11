d40fae28a477932a9010b76cfa616700207fbb12
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-11 12:22:00 -0700

    Some public key validity checks

--- check-balance-supply-pubkey.ts
+++ check-balance-supply-pubkey.ts
@@ -1,8 +1,19 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
 const suppliedPublicKey = process.argv[2];
+
 if (!suppliedPublicKey) {
     throw new Error("Provide a public key to check the balance of!");
+}
+
+// check that public key has between 43 and 44 characters
+if (suppliedPublicKey.length < 43 || suppliedPublicKey.length > 44) {
+    throw new Error("Invalid public key, Too short or Too long!");
+}
+
+// check that supplied public key is of type PublicKey
+if (!PublicKey.isOnCurve(suppliedPublicKey)) {
+    throw new Error("Invalid public key!");
 }
 
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
