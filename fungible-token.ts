2c535ba6f88af3d862267c501d93aa18e6c75ef9
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-14 16:40:31 -0700

    Get mint info, create associated account, mint to that account 100 tokens

--- fungible-token.ts
+++ fungible-token.ts
@@ -1,17 +1,16 @@
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
-import { createMint } from "@solana/spl-token";
+import { createMint, getMint, getOrCreateAssociatedTokenAccount, getAccount, mintTo } from "@solana/spl-token";
 import { clusterApiUrl, Connection, Keypair } from "@solana/web3.js";
 
 const payer = getKeypairFromEnvironment("SECRET_KEY");
-console.log(`Payer: ${payer}`);
+console.log(`Payer : ${payer.publicKey.toBase58()}`);
 const mintAuthority = Keypair.generate();
-console.log(`Mint Authority: ${mintAuthority}`);
+console.log(`Mint Authority: ${mintAuthority.publicKey.toBase58()}`);
 const freezeAuthority = Keypair.generate();
-console.log(`Freeze Authority: ${freezeAuthority}`);
+console.log(`Freeze Authority: ${freezeAuthority.publicKey.toBase58()}`);
 
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-console.log(`Connection: ${connection}`);
 
 const mint = await createMint(
     connection,
@@ -21,5 +20,33 @@ const mint = await createMint(
     9 // We are using 9 to match the CLI decimal default exactly
 );
 
-console.log(mint.toBase58());
-// AQoKYV7tYpTrFZN6P5oUufbQKAUr9mNYGe1TTJC9wajM
+console.log(`Mint: ${mint.toBase58()}`);
+
+const mintInfo = await getMint(connection, mint);
+
+console.log(`Mint Info Supply: ${mintInfo.supply}`);
+
+const tokenAccount = await getOrCreateAssociatedTokenAccount(connection, payer, mint, payer.publicKey);
+
+console.log(`Token Account: ${tokenAccount.address.toBase58()}`);
+
+const tokenAccountInfo = await getAccount(connection, tokenAccount.address);
+
+console.log(`Token Account Info: ${tokenAccountInfo.amount}`);
+
+await mintTo(
+    connection,
+    payer,
+    mint,
+    tokenAccount.address,
+    mintAuthority,
+    100000000000 // because decimals for the mint are set to 9
+);
+
+const updatedMintInfo = await getMint(connection, mint);
+
+console.log(`Updated Mint Info Supply: ${updatedMintInfo.supply}`);
+
+const updatedTokenAccountInfo = await getAccount(connection, tokenAccount.address);
+
+console.log(`Updated Token Account Info: ${updatedTokenAccountInfo.amount}`);
