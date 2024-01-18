0874b5fa5b9e246a25049fe61e2951f09b63d329
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-17 20:13:00 -0700

    Clean up

--- transfer-tokens.ts
+++ transfer-tokens.ts
@@ -4,25 +4,11 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 (async () => {
-    // Connect to cluster
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-    // Generate a new wallet keypair and airdrop SOL
-    // const fromWallet = Keypair.generate();
-    // const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
 
     const fromWallet = getKeypairFromEnvironment("SECRET_KEY");
 
-    // Wait for airdrop confirmation
-    // await connection.confirmTransaction(fromAirdropSignature);
-
-    // Generate a new wallet to receive newly minted token
-    // const toWallet = Keypair.generate();
-
     const toWallet = getKeypairFromEnvironment("SECRET_KEY_1");
-
-    // Create new token mint
-    // const mint = await createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
 
     // Use existing token mint
     const mint = new PublicKey("7yrcWPayujif46vPFM9TpcqkBL1SnrFHN6pDLN3MbjsB");
@@ -35,27 +21,8 @@ import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
         fromWallet.publicKey
     );
 
-    // Get the token account of the toWallet address, and if it does not exist, create it
     const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey);
 
-    // Mint 1 new token to the "fromTokenAccount" account we just created
-    // let signature = await mintTo(
-    //     connection,
-    //     fromWallet,
-    //     mint,
-    //     fromTokenAccount.address,
-    //     fromWallet.publicKey,
-    //     1000000000
-    // );
-    // console.log("mint tx:", signature);
-
-    // const amountInTokens = 9.99999999; // Amount in tokens you want to transfer
-    // let amountInSmallestUnit = amountInTokens * Math.pow(10, 9); // Convert to smallest unit, decimal precision of mint is 9
-    // // Round to the nearest integer
-    // amountInSmallestUnit = Math.round(amountInSmallestUnit); // 9999999990
-    // console.log(`Amount in smallest unit: ${amountInSmallestUnit}`);
-
-    // Transfer the new token to the "toTokenAccount" we just created
     await transfer(
         connection,
         fromWallet,
